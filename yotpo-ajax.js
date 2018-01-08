bb54de308fc734b6c30a4a2de9071e1e264d270d
Leoncio <leoncioperez512@gmail.com>
2018-01-08 01:34:16 -0500

    added more features and ease of use

--- yotpo-ajax.js
+++ yotpo-ajax.js
@@ -10,10 +10,35 @@ $(document).ready(function() {
 	// credentials to login first.
 	// ===================================
 
+	// ==============================================
+	// Specify the platform_type_id youre using. 
+	// Shopify is 1,
+	// Bigcommerce is 3,
+	// Magento is 5
+	// ==============================================
+	// You can find the shop domain above the API keys
+	// in Yotpo's account settings.
+	// ============================================== 
+
+	// ===================================
+	// Global Control Center
+	// In Google We Trust
+	// ===================================
+	var client_api_key = "CLIENT_API_KEY";
+	var client_secret = "CLIENT_API_SECRET";
+	var platform_type_id = 1;
+	var shop_domain = "https://CLIENT.WEBSITE.COM";
+
+	// Number of reviews to pull
+	var review_count = 40;
+	
+
+	// ====================================================
+	// Init Yotpo API Auth
 	var yotpoAuthUrl = 'https://api.yotpo.com/oauth/token',
 			credentials = {
-				"client_id": "CLIENT_API_KEY",
-				"client_secret": "CLIENT_API_SECRET",
+				"client_id": client_api_key,
+				"client_secret": client_secret,
 				"grant_type": "client_credentials"
 			},
 			accessToken = '',
@@ -28,15 +53,51 @@ $(document).ready(function() {
 				}
 			});
 
-	// ==============================================
-	// After auth, specify the platform_type_id youre using. 
-	// Shopify is 1,
-	// Bigcommerce is 3,
-	// Magento is 5
-	// ==============================================
-	// You can find the shop domain above the API keys
-	// in Yotpo's account settings.
-	// ==============================================
+	// ==================================
+	// Prerequisites
+	// ==================================
+
+	// Format Review Date
+	function formatDate(date) {
+    var month = date.getMonth(),
+        day = date.getDate().toString(),
+        year = date.getFullYear();
+
+    year = year.toString().substr(-2);
+    month = (month + 1).toString();  
+    return month + "/" + day + "/" + year;
+  }
+
+  // Humanize Strings
+  function humanize(str) {
+  	var frags = str.split('_');
+  	for (i=0; i<frags.length; i++) {
+  		frags[i] = frags[i].charAt(0).toUpperCase() + frags[i].slice(1);
+  	}
+  	return frags.join(' ');
+  }
+
+  // Star Rating
+  // ===================================================================
+  // 
+  // For CSS in half star, use :after element with absolute positioning
+  // HTML Use
+  // <span class="stars" data-rating="review.score" data-num-stars="5"></span>
+  // JS Call
+  // $('.stars').stars();
+
+  $.fn.stars = function () {
+
+  	return $(this).each(function() {
+  		var rating = $(this).data('rating'),
+  				numStars = $(this).data('num-stars'),
+  				fullStar = new Array(Math.floor(rating + 1)).join('<i class="fa fa-star"></i>'),
+  				halfStar = ((rating%1) !== 0) ? '<i class="fa fa-star-half"></i>': '',
+  				noStar = new Array(Math.floor(numStars + 1 - rating)).join('<i class="fa fa-star empty"></i>');
+
+  		$(this).html(fullStar + halfStar + noStar);
+  	});
+  }
 
 	initYotpoAuth.done(function(data) {
 		// Get access token. You will need this when accessing the API
@@ -45,31 +106,57 @@ $(document).ready(function() {
 		// Data for AJAX request
 		var data = {
 			"account_platform": {
-				"platform_type_id": 1,
-				"shop_domain": "https://CLIENT.WEBSITE.COM"
+				"platform_type_id": platform_type_id,
+				"shop_domain": shop_domain
 			},
 			"utoken": accessToken
 		};
 
-		// You will also need your api key per request. 
-		var appKey = 'CLIENT_API_KEY';
+		// Reviews Vote Calls
+		// ** Call inside $.each **
+		function yotpoReviewVote(review_id, vote_type) {
+			$.ajax({
+				url: 'https://api.yotpo.com/reviews/'+review_id+'/vote/'+vote_type+'',
+				type: 'POST',
+				data: JSON.stringify(data),
+				dataType: 'json',
+				contentType: 'application/json',
+				success: function() {
+					console.log('Review id: '+review_id+', Voted: '+vote_type+'');
+				}
+			});
+		}
 
-		// For getting reviews, specify review count
-		var count = 40;
-
-		// Begin querying! This example is for getting the latest 40 Reviews
+		// Reviews query
 		$.ajax({
-			url: 'https://api.yotpo.com/v1/apps/'+appKey+'/reviews?utoken='+accessToken+'&count='+count+'',
+			url: 'https://api.yotpo.com/v1/apps/'+client_api_key+'/reviews?utoken='+accessToken+'&count='+review_count+'',
 			type: 'GET',
 			data: JSON.stringify(data),
 			dataType: 'json',
 			contentType: 'application/json',
 			success: function(response) {
+
 				// See the json on your log!
 				console.log(response);
 
-				// Do whatever else you want below!
+				// GET data for each response
+				$.each( response.reviews, function(i, review) {
+					// Review Variables 
+					var reviewTitle = review.title,
+              reviewContent = review.content,
+              reviewDate = new Date(review.created_at),
+              reviewDate = formatDate(reviewDate),
+              reviewScore = review.score,
+              reviewTotalScore = 5,
+              reviewVoteUp = review.votes_up,
+              reviewVoteDown = review.votes_down,
+              reviewTotalVote = (reviewVoteUp - reviewVoteDown);
+              reviewName = review.name,
+              reviewId = review.id,
+              reviewType = review.reviewer_type,
+              reviewType = humanize(reviewType);
 
+				});
 			}
 		});
 	});
